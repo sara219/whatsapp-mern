103b28ec19474faf36a707b332cc08e320974c76
sara219 <sara97abuawwad@gmail.com>
2020-09-27 23:02:38 +0300

    added the pusher into the client side and run it

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css'
 import { Chat } from './component/Chat';
 import { Sidebar } from './component/Sidebar';
+import Pusher from 'pusher-js'
 
 function App() {
+
+  useEffect(() => {
+    // connects to Channels and subscribes to a channel called messages, listening for an event called inserted.
+    const pusher = new Pusher('a43f72fa78eb808b505d', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('message');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className='app'>
       <div className='app__body'>
